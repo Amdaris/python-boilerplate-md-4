8448774ea750bff3e0e4900c6aa66d1797a1c409
Oleg Rotar <oleg.rotar@amdaris.com>
2025-03-08 22:46:41 +0200

    Update challenges

---
+++ app/app/challenges.py
@@ -0,0 +1,14 @@
+def challenge1(array):
+    # Write the code for Challenge 1 here
+    raise NotImplementedError("Not implemented")
+
+def challenge2(array):
+    # Write the code for Challenge 1 here
+    raise NotImplementedError("Not implemented")
+
+def challenge3(array):
+    # Write the code for Challenge 1 here
+    raise NotImplementedError("Not implemented")
+
+
+
--- app/app/views.py
+++ app/app/views.py
@@ -1,7 +1,5 @@
-from django.http import JsonResponse, HttpResponse
-import json
+from app.challenges import challenge1, challenge2, challenge3
 
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,91 +11,59 @@ class PingApiView(APIView):
         return Response({"result": "pong"})
 
 class Challenge1APIView(APIView):
-    @swagger_auto_schema(request_body=openapi.Schema(
-        type=openapi.TYPE_ARRAY,
-        items=openapi.Items(type=openapi.TYPE_INTEGER),
-    ))
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Items(type=openapi.TYPE_INTEGER),
+        ),
+        responses={
+            200: openapi.Schema(
+                type=openapi.TYPE_BOOLEAN,
+            )
+        }
+    )
     def post(self, request):
         array = request.data
 
-        # Write the code for Challenge 1 here
-
-        n = len(array)
+        result = challenge1(array)
         
-        for i in range(n):
-            if array[i] <= 0 or array[i] > n:
-                array[i] = 0
-        
-        for i in range(n):
-            val = abs(array[i])
-            if 0 < val <= n:
-                if array[val - 1] > 0:
-                    array[val - 1] = -array[val - 1]
-                elif array[val - 1] == 0:
-                    array[val - 1] = -(n + 1)
-        
-        for i in range(n):
-            if array[i] >= 0:
-                return Response(i + 1)
-        
-        return Response(n + 1)
+        return Response(result)
 
 class Challenge2APIView(APIView):
-    @swagger_auto_schema(request_body=openapi.Schema(
-        type=openapi.TYPE_ARRAY,
-        items=openapi.Items(type=openapi.TYPE_INTEGER),
-    ))
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Items(type=openapi.TYPE_INTEGER),
+        ),
+        responses={
+            200: openapi.Schema(
+                type=openapi.TYPE_ARRAY,
+                items=openapi.Items(type=openapi.TYPE_INTEGER),
+            )
+        }
+    )
     def post(self, request):
         array = request.data
 
-        # Write the code for Challenge 2 here
-
-        if len(array) == 0:
-            return Response([])
+        result = challenge2(array)
         
-        count = 1
-        for i in range(1, len(array)):
-            if array[i] != array[i - 1]:
-                count += 1
-        
-        result = [0] * count
-        index = 0
-        sum_val = array[0]
-        
-        for i in range(1, len(array)):
-            if array[i] == array[i - 1]:
-                sum_val += array[i]
-            else:
-                result[index] = sum_val
-                index += 1
-                sum_val = array[i]
-        
-        result[index] = sum_val
         return Response(result)
 
 class Challenge3APIView(APIView):
-    @swagger_auto_schema(request_body=openapi.Schema(
-        type=openapi.TYPE_ARRAY,
-        items=openapi.Items(type=openapi.TYPE_INTEGER),
-    ))
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_ARRAY,
+            items=openapi.Items(type=openapi.TYPE_INTEGER),
+        ),
+        responses={
+            200: openapi.Schema(
+                type=openapi.TYPE_INTEGER,
+            )
+        }
+    )
     def post(self, request):
         array = request.data
 
-        # Write the code for Challenge 3 here
-
-        if len(array) == 0:
-            return Response(False)
+        result = challenge3(array)
         
-        min_val = min(array)
-        max_val = max(array)
-        
-        if max_val - min_val + 1 != len(array):
-            return Response(False)
-        
-        seen = set()
-        for num in array:
-            if num in seen:
-                return Response(False)
-            seen.add(num)
-
-        return Response(True)
+        return Response(result)
